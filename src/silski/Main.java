9f6aa6550acb8c446b772afd4296193918422d84
patnjacq <yan20004@byui.edu>
2021-01-30 09:35:39 -0800

    Hello from Jacque on a new line

--- src/silski/Main.java
+++ src/silski/Main.java
@@ -3,7 +3,7 @@ package silski;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello, my name is Michael");
-        System.out.println("Hello Micahael, my name is Hadassah.");
+        System.out.println("Hello, my name is Michael");
+        System.out.println("Hello Michael, my name is Jacque");
     }
 }
