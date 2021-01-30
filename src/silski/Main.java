1c42cdd80acf08929dddf723520a9524fb09c21b
MichaelSilski <sil16005@byui.edu>
2021-01-30 12:44:36 -0700

    Making changes to test the Stretch Challenge

--- src/silski/Main.java
+++ src/silski/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Michael");
         System.out.println("Hello Michael, my name is Jacque");
+        System.out.println("Making changes to test the Stretch Challenge");
     }
 }
