5cb199e82f4da48748a66d68000192ca7ffaa233
MichaelSilski <sil16005@byui.edu>
2021-01-29 21:30:56 -0700

    Initial commit, adding necessary files to GitHub

--- src/silski/Main.java
+++ src/silski/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.print("Hello, my name is Michael");
+        System.out.println("Hello Micahael, my name is Hadassah.");
     }
 }
